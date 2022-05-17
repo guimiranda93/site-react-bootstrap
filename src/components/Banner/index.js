9ff17cde703854b245cb048701ca4f213e45090f
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2022-05-16 21:04:49 -0300

    First commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import bg from "../../img/hero-bg.png";
+
+const Banner = () => {
+	return (
+		<div>
+			<img className="d-block w-100" src={bg} alt="First slide" />
+		</div>
+	);
+};
+
+export default Banner;
